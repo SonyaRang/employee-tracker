2e4207fc644ab2dc4f7f9cef94ad873dcde25ca0
Sonz <sonyarangraj@msn.com>
2021-06-13 22:53:07 -0500

    first commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,25 @@
---database
-DROP DATABASE IF EXISTS employee_tracker;
-CREATE DATABASE employee_tracker;
-USE employee_tracker;
-
---tables departments 
-CREATE TABLE Departments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+--Departments
+CREATE TABLE departments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
---table roles
-CREATE TABLE Roles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT
+--Roles
+CREATE TABLE roles(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) 
+    REFERENCES departments(id)
 );
 
---table employee 
-CREATE TABLE Employees (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT
+--Employees 
+CREATE TABLE employees(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    role_id INT NOT NULL
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,17 @@
--- dont need to add the ID as the system will auto increment
+--Departments
 INSERT INTO Departments (name)
 VALUES ('CEO'), ('Sales'), ('Financing'), ('Marketing');
 
-INSERT INTO Roles (title, salary, department_id)
+--Roles
+INSERT INTO Roles (Title, Salary, Department_id)
 VALUES 
     ('CEO', 120000.00, 01),
     ('Sales', 80000.00, 02),
     ('Financing', 90000.00, 03),
     ('Marketing', 100000, 04);
 
-INSERT INTO Employees (first_name, last_name, role_id, manager_id)
+--Employees
+INSERT INTO Employees (First_name, Last_name, Role_id, Manager_id)
 VALUES 
     ('Sonya', 'Rangraj', 01, null),
     ('Harry', 'Potter', 02, 04),
